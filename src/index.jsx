a777b4c48ca5077292e71e2a3a8a08a6f002455a
wdotmathree <wdotmathree@gmail.com>
2022-09-30 12:04:40 -0600

    First working version with images

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,21 @@ import BubbleChart from "./react-bubble-chart-d3";
 import team from "./team";
 import "./style.scss";
 
+class InfoBar extends React.Component {
+	render() {
+		return (
+			<div className="info-bar">
+				<div className="info-bar__title">
+					<h1>React Bubble Chart D3</h1>
+					<h2>
+						by <a href="">Vasturiano</a>
+					</h2>
+				</div>
+			</div>
+		);
+	}
+}
+
 class App extends React.Component {
 	constructor(props) {
 		super(props);
@@ -19,44 +34,22 @@ class App extends React.Component {
 			<div>
 				<BubbleChart
 					width={1000}
-					height={800}
+					height={1000}
 					padding={0} // optional value, number that set the padding between bubbles
 					showLegend={true} // optional value, pass false to disable the legend.
 					legendPercentage={0} // number that represent the % of with that legend going to use.
-					legendFont={{
-						family: "Arial",
-						size: 12,
-						color: "#000",
-						weight: "bold",
-					}}
 					labelFont={{
 						family: "Arial",
-						size: 16,
+						size: 36,
 						color: "#fff",
 						weight: "bold",
 					}}
 					//Custom bubble/legend click functions such as searching using the label, redirecting to other page
 					bubbleClickFunc={this.bubbleClick}
 					legendClickFun={this.legendClick}
-					data={[
-						{ label: "CRM", value: 1 },
-						{ label: "API", value: 1 },
-						{ label: "Data", value: 1 },
-						{ label: "Commerce", value: 1 },
-						{ label: "AI", value: 3 },
-						{ label: "Management", value: 5 },
-						{ label: "Testing", value: 6 },
-						{ label: "Mobile", value: 9 },
-						{ label: "Conversion", value: 9 },
-						{ label: "Misc", value: 21 },
-						{ label: "Databases", value: 22 },
-						{ label: "DevOps", value: 22 },
-						{ label: "Javascript", value: 23 },
-						{ label: "Languages / Frameworks", value: 25 },
-						{ label: "Front End", value: 26 },
-						{ label: "Content", value: 26 },
-					]}
+					data={team}
 				/>
+				<InfoBar></InfoBar>
 			</div>
 		);
 	}
